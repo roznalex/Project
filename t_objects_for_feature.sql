d7c0aa8b4403bbe373ddb71831c317e074042a9c
Ivan Melnyk <melnyk.ivan@hotmail.com>
2014-12-14 01:45:53 +0200

    Debugged t_objects
    Це ппц !
    Number завжди має мати RANGE!!!
    дефолт у типах не прописується!!!

--- t_objects_for_feature.sql
+++ t_objects_for_feature.sql
@@ -1,28 +1,29 @@
 CREATE OR REPLACE TYPE T_STATE AS OBJECT
-	(publish       NUMBER(0,1),
-	 last_record   NUMBER(0,1),
-	 was_published NUMBER(0,1),
-	 status_id     NUMBER,
-	 last_action   NUMBER);
-	 
+	(publish       NUMBER(1),
+	 last_record   NUMBER(1),
+	 was_published NUMBER(1),
+	 status_id     NUMBER(10),
+	 last_action   NUMBER(10));
+/	 
 CREATE OR REPLACE TYPE T_FEATURE AS OBJECT 
-	(feature_id			NUMBER,
-	 group_id 			NUMBER,
-	 feature_type_id	NUMBER, 
+	(feature_id			NUMBER(5),
+	 group_id 			NUMBER(5),
+	 feature_type_id	NUMBER(5), 
 	 feature_value		VARCHAR2(200), 
 	 description 		VARCHAR2(4000), 
-	 valid_start_date   TIMESTAMP(6) WITH LOCAL TIMEZONE,
-	 status_id 			NUMBER,
-	 last_action_id		NUMBER,
-	 publish 			NUMBER(0,1),
-	 last_record        NUMBER(0,1),
-	 linked             NUMBER(0,1),
-	 was_published      NUMBER(0,1),
+	 valid_start_date   TIMESTAMP(6) WITH LOCAL TIME ZONE,
+	 status_id 			NUMBER(4),
+	 last_action_id		NUMBER(9),
+	 publish 			NUMBER(1),
+	 last_record        NUMBER(1),
+	 linked             NUMBER(1),
+	 was_published      NUMBER(1),
 	 comments           VARCHAR2(4000),
-	 active_flag        NUMBER(0,1),
+	 active_flag        NUMBER(1),
 	 last_modified_by   VARCHAR2(200),
-	 last_modified_date TIMESTAMP(6) WITH LOCAL TIMEZONE,
+	 last_modified_date TIMESTAMP(6) WITH LOCAL TIME ZONE,
 	 created_by         VARCHAR2(200),
-	 created_date       TIMESTAMP(6) WITH LOCAL TIMEZONE,
-	 is_default         NUMBER(0,1)                      DEFAULT 1,
-	 is_editable        NUMBER(0,1)                      DEFAULT 1);
+	 created_date       TIMESTAMP(6) WITH LOCAL TIME ZONE,
+	 is_default         NUMBER(1),
+	 is_editable        NUMBER(1));
+   /
