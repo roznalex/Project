83da8969e1a0e749caae432d3838a3d99ab89ec4
Sapyanov <anton.sapyanoff@yandex.ua>
2014-12-14 22:53:21 +0200

    Debugged product-procedures have been added

--- t_objects_for_product.sql
+++ t_objects_for_product.sql
@@ -1,31 +1,33 @@
-CREATE OR REPLACE TYPE T_STATE AS OBJECT
-	(publish       NUMBER(0,1),
-	 last_record   NUMBER(0,1),
-	 was_published NUMBER(0,1),
-	 status_id     NUMBER,
-	 last_action   NUMBER);
+create or replace TYPE T_STATE AS OBJECT 
+(  	 publish        NUMBER(1),
+	 last_record   	NUMBER(1),
+	 was_published 	NUMBER(1),
+	 status_id     	NUMBER(5),
+	 last_action_id NUMBER(5)
+);
 	 
-CREATE OR REPLACE TYPE T_PRODUCT AS OBJECT 
-	(product_id			NUMBER,
-	 group_id 			NUMBER,
-	 product_uid		NUMBER, 
-	 product_name		VARCHAR2(150), 
-	 product_long_name	VARCHAR2(250), 
-	 description		VARCHAR2(4000),
-	 valid_start_date   TIMESTAMP(6) WITH LOCAL TIMEZONE,
-	 status_id 			NUMBER,
-	 last_action_id		NUMBER,
-	 publish 			NUMBER(0,1),
-	 last_record        NUMBER(0,1),
-	 linked             NUMBER(0,1),
-	 was_published      NUMBER(0,1),
-	 comments           VARCHAR2(4000),
-	 active_flag        NUMBER(0,1),
-	 last_modified_by   VARCHAR2(200),
-	 last_modified_date TIMESTAMP(6) WITH LOCAL TIMEZONE,
-	 created_by         VARCHAR2(200),
-	 created_date       TIMESTAMP(6) WITH LOCAL TIMEZONE
-	 lnk_feature		TBL_LNK_FEATURE);
+create or replace TYPE T_PRODUCT AS OBJECT 
+(product_id			      	NUMBER,
+	 group_id 			    NUMBER,
+	 product_uid		    NUMBER, 
+	 product_name		    VARCHAR2(150), 
+	 product_long_name		VARCHAR2(250), 
+	 description		    VARCHAR2(4000),
+	 valid_start_date   	TIMESTAMP(6) WITH LOCAL TIME ZONE,
+	 status_id 			    NUMBER,
+	 last_action_id		  	NUMBER,
+	 publish 			    NUMBER(1),
+	 last_record        	NUMBER(1),
+	 linked             	NUMBER(1),
+	 was_published      	NUMBER(1),
+	 comments           	VARCHAR2(4000),
+	 active_flag        	NUMBER(1),
+	 last_modified_by   	VARCHAR2(200),
+	 last_modified_date 	TIMESTAMP(6) WITH LOCAL TIME ZONE,
+	 created_by         	VARCHAR2(200),
+	 created_date       	TIMESTAMP(6) WITH LOCAL TIME ZONE,
+	 lnk_feature		    TBL_LNK_FEATURE
+);
 
 CREATE OR REPLACE TYPE T_LNK_FEATURE AS OBJECT
 	(feature_id			NUMBER,
@@ -37,3 +39,16 @@ CREATE SEQUENCE seq_lnk_prod_feat_id
 		INCREMENT BY 1
 		START WITH 1
 		CACHE 20;
+
+CREATE SEQUENCE SEQ_PROD_GROUP_ID
+		INCREMENT BY 1 
+		START WITH 1 
+		CACHE 20;
+
+   CREATE SEQUENCE SEQ_PROD_PRODUCT_ID
+		INCREMENT BY 1 
+		START WITH 1 
+		CACHE 20;
+ 
+
+ 
