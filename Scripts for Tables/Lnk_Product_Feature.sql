6d1a7bff2cfd8e0af5f1f43fc6636ddb433cd310
Kemstan <kemstan@mail.ru>
2014-12-14 15:58:26 +0200

    Update Lnk_Product_Feature.sql

--- Scripts for Tables/Lnk_Product_Feature.sql
+++ Scripts for Tables/Lnk_Product_Feature.sql
@@ -7,7 +7,7 @@ last_modified_by             varchar2(200),
 last_modified_date           timestamp(6) with local time zone,
 created_by                   varchar2(200),
 created_date                 timestamp(6)  with local time zone
-;
+);
 
 
 ALTER TABLE LNK_PRODUCT_FEATURE
