2cd5a7bdc0c17435e4771c5594a0cc7e0a18046b
antin-ua <antin.kyseliuk@gmail.com>
2014-12-16 14:44:35 +0200

    one more fixes..

--- Filters/filter.sql
+++ Filters/filter.sql
@@ -1,20 +1,21 @@
 
 CREATE TYPE Varchar_tab IS TABLE OF VARCHAR2(200);
+/
 CREATE TYPE Integer_tab IS TABLE OF INTEGER;
-
+/
 CREATE OR REPLACE TYPE g_filter_obj AS (
     publish             NUMBER(1),
     linked              NUMBER(1),
     latestrecord        NUMBER(1),
     status_multiselect  Varchar_tab,
-    user                Varchar_tab,
+    users               Varchar_tab,
     MEMBER PROCEDURE get_latestrec_for_f(value_out OUT Feature%ROWTYPE),
     MEMBER PROCEDURE get_latestrec_for_p(value_out OUT Product%ROWTYPE),
     MEMBER PROCEDURE get_active_statuses(value_out OUT Varchar_tab),
     MEMBER PROCEDURE get_last_action(value_out OUT Last_action%ROWTYPE),
-    MEMBER PROCEDURE get_all_users(from_in IN VARCHAR2(7), value_out OUT Varchar_tab)
-)
-
+    MEMBER PROCEDURE get_all_users(from_in IN VARCHAR2, value_out OUT Varchar_tab)
+);
+/
 CREATE OR REPLACE TYPE f_filter_obj AS (
     grid_filter_obj     g_filter_obj,
     feature_type        VARCHAR2(200),
@@ -23,22 +24,22 @@ CREATE OR REPLACE TYPE f_filter_obj AS (
     MEMBER PROCEDURE get_active_fts(value_out OUT Varchar_tab),
     MEMBER PROCEDURE get_uq_fvs(value_out OUT Varchar_tab),
     MEMBER PROCEDURE get_uq_gids(value_out OUT Integer_tab)
-)
-
+);
+/
 CREATE OR REPLACE TYPE p_filter_obj AS (
     grid_filter_obj     g_filter_obj,
     feature_value       VARCHAR2(200),
     product_uid         INTEGER,
     product_name        VARCHAR2(150),
     product_long_name   VARCHAR2(250),
-    MEMBER PROCEDURE get_uq_names(value_out OUT Varcah_tab, length_in IN VARCHAR2(5)),
+    MEMBER PROCEDURE get_uq_names(value_out OUT Varcah_tab, length_in IN VARCHAR2),
     MEMBER PROCEDURE get_fvs_from_product(value_out OUT Varcahr_tab),
     MEMBER PROCEDURE get_uq_gids(value_out OUT Integer_tab)
-)
-
+);
+/
 CREATE OR REPLACE TYPE BODY g_filter_obj AS
 
-    MEMBER PROCEDURE get_latestrec_for_f (value_out OUT Feature%ROWTYPE)
+    MEMBER PROCEDURE get_latestrec_for_f (value_out OUT Feature%ROWTYPE) IS
     BEGIN
         EXECUTE IMMEDIATE
         'SELECT *'
@@ -46,21 +47,21 @@ CREATE OR REPLACE TYPE BODY g_filter_obj AS
         || Feature
         || ' WHERE '
         || 'last_record = 1'
-        INTO value_out
-    END latestrec;
+        INTO value_out;
+    END get_latestrec_for_f;
 
-    MEMBER PROCEDURE get_latestrec_for_p (value_out OUT Product%ROWTYPE)
+    MEMBER PROCEDURE get_latestrec_for_p (value_out OUT Product%ROWTYPE) IS
     BEGIN
-    EXECUTE IMMEDIATE
-    'SELECT *'
-    || ' FROM '
-    || Product
-    || ' WHERE '
-    || 'last_record = 1'
-    INTO value_out
-    END latestrec;
+        EXECUTE IMMEDIATE
+        'SELECT *'
+        || ' FROM '
+        || Product
+        || ' WHERE '
+        || 'last_record = 1'
+        INTO value_out;
+    END get_latestrec_for_p;
 
-    MEMBER PROCEDURE all_active (value_out OUT Varchar_tab)
+    MEMBER PROCEDURE all_active (value_out OUT Varchar_tab) IS
     BEGIN
         EXECUTE IMMEDIATE
         'SELECT '
@@ -72,7 +73,7 @@ CREATE OR REPLACE TYPE BODY g_filter_obj AS
         BULK COLLECT INTO value_out;
     END all_active;
 
-    MEMBER PROCEDURE get_last_action(value_out OUT Last_action%ROWTYPE)
+    MEMBER PROCEDURE get_last_action(value_out OUT Last_action%ROWTYPE) IS
     BEGIN
         EXECUTE IMMEDIATE
         'SELECT *'
@@ -80,11 +81,11 @@ CREATE OR REPLACE TYPE BODY g_filter_obj AS
         || Last_action
         || ' WHERE '
         || 'last_modified_date = MAX(last_modified_date)'
-        INTO value_out
+        INTO value_out;
     END get_last_action;
 
-    MEMBER PROCEDURE get_all_users(from_in IN VARCAHR2(7),
-                                    value_out OUT Varchar_tab)
+    MEMBER PROCEDURE get_all_users(from_in IN VARCHAR2,
+                                    value_out OUT Varchar_tab) IS
     BEGIN
         EXECUTE IMMEDIATE
         'SELECT DISTINCT'
@@ -94,9 +95,9 @@ CREATE OR REPLACE TYPE BODY g_filter_obj AS
         BULK COLLECT INTO value_out;
     END get_all_users;
 END;
-
+/
 CREATE OR REPLACE TYPE BODY f_filter_obj AS
-    MEMBER PROCEDURE get_active_fts(value_out OUT Varchar_tab)
+    MEMBER PROCEDURE get_active_fts(value_out OUT Varchar_tab) IS
     BEGIN
         EXECUTE IMMEDIATE
         'SELECT '
@@ -108,7 +109,7 @@ CREATE OR REPLACE TYPE BODY f_filter_obj AS
         BULK COLLECT INTO value_out;
     END get_active_fts;
 
-    MEMBER PROCEDURE get_uq_fvs(value_out OUT Varchar_tab)
+    MEMBER PROCEDURE get_uq_fvs(value_out OUT Varchar_tab) IS
     BEGIN
         EXECUTE IMMEDIATE
         'SELECT DISTINCT'
@@ -118,7 +119,7 @@ CREATE OR REPLACE TYPE BODY f_filter_obj AS
         BULK COLLECT INTO value_out;
     END get_uq_fvs;
 
-    MEMBER PROCEDURE get_uq_gids(value_out OUT Integer_tab)
+    MEMBER PROCEDURE get_uq_gids(value_out OUT Integer_tab) IS
     BEGIN
         EXECUTE IMMEDIATE
         'SELECT DISTINCT'
@@ -126,11 +127,11 @@ CREATE OR REPLACE TYPE BODY f_filter_obj AS
         || ' FROM '
         || Feature
         BULK COLLECT INTO value_out;
-    END get_ua_gids;
+    END get_uq_gids;
 END;
-
+/
 CREATE OR REPLACE TYPE BODY p_filter_obj AS
-    MEMBER PROCEDURE get_uq_gids(value_out OUT Integer_tab)
+    MEMBER PROCEDURE get_uq_gids(value_out OUT Integer_tab) IS
     BEGIN
         EXECUTE IMMEDIATE
         'SELECT DISTINCT'
@@ -138,27 +139,28 @@ CREATE OR REPLACE TYPE BODY p_filter_obj AS
         || ' FROM '
         || Product
         BULK COLLECT INTO value_out;
-    END get_ua_gids;
+    END get_uq_gids;
 
-    MEMBER PROCEDURE get_uq_names(value_out OUT Varcah_tab, length_in IN VARCHAR2(5))
+    MEMBER PROCEDURE get_uq_names(value_out OUT Varcah_tab, length_in IN VARCHAR2) IS
     BEGIN
-    IF length_in = "short" THEN
-        EXECUTE IMMEDIATE
-        'SELECT DISTINCT'
-        || product_name
-        || ' FROM '
-        || Product
-        BULK COLLECT INTO value_out;
-    ELSE
-        EXECUTE IMMEDIATE
-        'SELECT DISTINCT'
-        || product_long_name
-        || ' FROM '
-        || Product
-        BULK COLLECT INTO value_out;
+        IF length_in = 'short' THEN
+            EXECUTE IMMEDIATE
+            'SELECT DISTINCT'
+            || product_name
+            || ' FROM '
+            || Product
+            BULK COLLECT INTO value_out;
+        ELSE
+            EXECUTE IMMEDIATE
+            'SELECT DISTINCT'
+            || product_long_name
+            || ' FROM '
+            || Product
+            BULK COLLECT INTO value_out;
+        END IF;
     END get_uq_names;
 
-    MEMBER PROCEDURE get_fvs_from_product(value_out OUT Varcahr_tab)
+    MEMBER PROCEDURE get_fvs_from_product(value_out OUT Varcahr_tab) IS
     BEGIN
         EXECUTE IMMEDIATE
         'SELECT '
@@ -172,7 +174,7 @@ CREATE OR REPLACE TYPE BODY p_filter_obj AS
     END get_fvs_from_product;
 
 END;
-
+/
 CREATE OR REAPLACE PROCDURE f_filter ( f_filter_obj_in IN f_filter_obj,
                                         cur_out OUT SYS_REFCURSOR)
 IS
@@ -220,14 +222,14 @@ BEGIN
                                 || f_filter_obj_in.g_filter_obj.last_action || ') AND ';
         END IF;
 
-        IF f_filter_obj_in.g_filter_obj.user IS NOT NULL THEN
+        IF f_filter_obj_in.g_filter_obj.users IS NOT NULL THEN
             v_temp := '(';
-            FOR idx IN f_filter_obj_in.g_filter_obj.user.FIRST ..
-                        (f_filter_obj_in.g_filter_obj.user.LAST - 1)
+            FOR idx IN f_filter_obj_in.g_filter_obj.users.FIRST ..
+                        (f_filter_obj_in.g_filter_obj.users.LAST - 1)
             LOOP
                 v_temp := v_temp || f_filter_obj_in.g_filter_obj.user(idx) || ',';
             END LOOP;
-            v_temp := v_temp || f_filter_obj_in.g_filter_obj.user(f_filter_obj_in.g_filter_obj.user.LAST) || ')';
+            v_temp := v_temp || f_filter_obj_in.g_filter_obj.users(f_filter_obj_in.g_filter_obj.users.LAST) || ')';
 
             v_sql := v_sql || 'last_modified_by IN ' || v_temp || ' AND ';
         END IF;
@@ -252,8 +254,8 @@ BEGIN
 
 END;
 
-
-CREATE OR REAPLACE PROCDURE f_filter ( p_filter_obj_in IN p_filter_obj,
+/
+CREATE OR REAPLACE PROCDURE p_filter ( p_filter_obj_in IN p_filter_obj,
                                         cur_out OUT SYS_REFCURSOR)
 IS
 
@@ -264,8 +266,8 @@ IS
 BEGIN
 
     IF p_filter_obj_in.g_filter_obj.latestrecord = 1 THEN
-        p_ilter_obj_in.g_filter_obj.latestrec(l_feature_piece);
-        OPEN cur_out FOR l_feature_piece;
+        p_ilter_obj_in.g_filter_obj.latestrec(l_product_piece);
+        OPEN cur_out FOR l_product_piece;
         CLOSE cur_out;
     ELSE
         v_sql := 'SELECT T_PRODUCT';
@@ -274,7 +276,7 @@ BEGIN
 
         IF p_filter_obj_in.g_filter_obj.publish IS NOT NULL THEN
             v_sql := v_sql || 'publish = '
-                            || f_filter_obj_in.g_filter_obj.publish || ' AND ';
+                            || p_filter_obj_in.g_filter_obj.publish || ' AND ';
         END IF;
 
         IF p_filter_obj_in.g_filter_obj.linked IS NOT NULL THEN
@@ -300,14 +302,14 @@ BEGIN
                                 || p_filter_obj_in.g_filter_obj.last_action || ') AND ';
         END IF;
 
-        IF p_filter_obj_in.g_filter_obj.user IS NOT NULL THEN
+        IF p_filter_obj_in.g_filter_obj.users IS NOT NULL THEN
             v_temp := '(';
-            FOR idx IN p_filter_obj_in.g_filter_obj.user.FIRST ..
-                    (p_filter_obj_in.g_filter_obj.user.LAST - 1)
+            FOR idx IN p_filter_obj_in.g_filter_obj.users.FIRST ..
+                    (p_filter_obj_in.g_filter_obj.users.LAST - 1)
             LOOP
-                v_temp := v_temp || p_filter_obj_in.g_filter_obj.user(idx) || ',';
+                v_temp := v_temp || p_filter_obj_in.g_filter_obj.users(idx) || ',';
             END LOOP;
-            v_temp := v_temp || p_filter_obj_in.g_filter_obj.user(p_filter_obj_in.g_filter_obj.user.LAST) || ')';
+            v_temp := v_temp || p_filter_obj_in.g_filter_obj.users(p_filter_obj_in.g_filter_obj.users.LAST) || ')';
 
             v_sql := v_sql || 'last_modified_by IN ' || v_temp || ' AND ';
         END IF;
@@ -321,12 +323,12 @@ BEGIN
         END IF;
 
         IF p_filter_obj_in.product_long_name IS NOT NULL THEN
-            v_sql := v_sql || 'product_long_name = ' || p_filter_obj_in.product_long_name || ' AND '
+            v_sql := v_sql || 'product_long_name = ' || p_filter_obj_in.product_long_name || ' AND ';
         END IF;
 
         IF p_filter_obj_in.feature_value IS NOT NULL THEN
             v_sql := v_sql || 'product_id IN (SELECT product_id FROM LNK_Product_Feature WHERE
-                                feature_id = (SELECT feature_id FROM Feature WHERE feature_value = ' p_filter_obj_in.feature_value || ')) ;'
+                                feature_id = (SELECT feature_id FROM Feature WHERE feature_value = ' p_filter_obj_in.feature_value || '))';
         END IF;
 
         OPEN cut_out FOR v_sql;
